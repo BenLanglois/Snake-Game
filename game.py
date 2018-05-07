0f8edc3ba6d8e6112eba352ac7e3aab1e660beac
Ben Langlois <blanglois000@gmail.com>
2018-05-07 18:26:54 -0400

    Added HUD, border, and restart after death. 1-frame delay still exists.

--- game.py
+++ game.py
@@ -1,18 +1,28 @@
 import pygame, sys, random
 from pygame.locals import *
 
-# Constants/Settings -----------------------------------------------------------
+# Colours ----------------------------------------------------------------------
 
-HUMAN_PLAYER               = True
-SHOW_BOARD = HUMAN_PLAYER or True
-BOARD_SIZE                 = 32
-BOX_SIZE                   = 15
-FPS                        = 10
-SEED                       = 1
-WHITE                      = (255,255,255)
-GREY                       = (127,127,127)
-BLACK                      = (  0,  0,  0)
-RED                        = (255,  0,  0)
+WHITE      = (255,255,255)
+GREY       = (127,127,127)
+BLACK      = (  0,  0,  0)
+RED        = (255,  0,  0)
+LIGHT_BLUE = (109,158,237)
+
+# Settings ---------------------------------------------------------------------
+
+HUMAN_PLAYER  = True
+SHOW_BOARD    = True
+BOARD_SIZE    = 32
+BOX_SIZE      = 15
+HUD_HEIGHT    = 40
+HUD_COLOUR    = LIGHT_BLUE
+TEXT_SIZE     = 30
+TEXT_COLOUR   = BLACK
+BORDER_WIDTH  = 5
+BORDER_COLOUR = WHITE
+FPS           = 10
+SEED          = 1
 
 # Class definitions ------------------------------------------------------------
 
@@ -125,14 +135,61 @@ def reset_game():
     snek = Snake()
     apple = Food(snek)
 
+# Snake died, restart game
+def game_over():
+    if HUMAN_PLAYER:
+        death_screen_surf = pygame.Surface((BOARD_SIZE*BOX_SIZE/2, BOARD_SIZE*BOX_SIZE/2))
+        death_screen_surf.fill(HUD_COLOUR)
+        death_screen_rect = death_screen_surf.get_rect()
+        death_text_surf = hud_font.render("Press space to restart", True, TEXT_COLOUR)
+        death_text_rect = death_text_surf.get_rect()
+        death_text_rect.center = death_screen_rect.center
+        death_screen_surf.blit(death_text_surf, death_text_rect)
+        death_screen_rect.center = game_board.get_rect().center
+        game_board.blit(death_screen_surf, death_screen_rect)
+        screen.blit(game_board, (BORDER_WIDTH, HUD_HEIGHT+BORDER_WIDTH))
+        pygame.display.update()
+
+        restart = False
+        while not restart:
+            for event in pygame.event.get():
+                if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN and event.key == K_SPACE:
+                    reset_game()
+                    global last_press
+                    last_press = K_UP
+                    restart = True
+            pygame.display.update()
+            clock.tick(FPS)
+
+    else:
+        pass
+
+def update_hud(snake):
+    hud.fill(HUD_COLOUR)
+    score_text_surf = hud_font.render("SCORE: " + str(snake.length), True, TEXT_COLOUR)
+    score_text_rect = score_text_surf.get_rect()
+    score_text_rect.center = (BOARD_SIZE*BOX_SIZE/2, HUD_HEIGHT/2)
+    hud.blit(score_text_surf, score_text_rect)
+
+
 # Setup ------------------------------------------------------------------------
+
+SHOW_BOARD = HUMAN_PLAYER or SHOW_BOARD # Must SHOW_BOARD if HUMAN_PLAYER
 
 if SHOW_BOARD:
     pygame.init()
-    screen = pygame.display.set_mode((BOARD_SIZE*BOX_SIZE, BOARD_SIZE*BOX_SIZE))
+    screen = pygame.display.set_mode((BOARD_SIZE*BOX_SIZE+BORDER_WIDTH*2, BOARD_SIZE*BOX_SIZE+HUD_HEIGHT+BORDER_WIDTH*2))
+    game_board = pygame.Surface((BOARD_SIZE*BOX_SIZE, BOARD_SIZE*BOX_SIZE))
+    hud = pygame.Surface((BOARD_SIZE*BOX_SIZE+BORDER_WIDTH*2, HUD_HEIGHT))
     pygame.display.set_caption("Snake game")
     clock = pygame.time.Clock()
-    last_press = K_UP
+    hud_font = pygame.font.SysFont("Verdana", TEXT_SIZE)
+    screen.fill(BORDER_COLOUR)
+    if HUMAN_PLAYER:
+        last_press = K_UP
 
 reset_game()
 
@@ -141,19 +198,25 @@ reset_game()
 while True:
     if SHOW_BOARD:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYDOWN and event.key in (K_UP, K_DOWN, K_LEFT, K_RIGHT):
+            elif HUMAN_PLAYER and event.type == KEYDOWN and event.key in (K_UP, K_DOWN, K_LEFT, K_RIGHT):
                 last_press = event.key
 
     if snek.isAlive:
         snek.move(apple)
+    else:
+        game_over()
 
     if SHOW_BOARD:
-        screen.fill(BLACK)
-        snek.draw(screen)
-        apple.draw(screen)
+        update_hud(snek)
+        game_board.fill(BLACK)
+        snek.draw(game_board)
+        apple.draw(game_board)
+
+        screen.blit(hud, (0, 0))
+        screen.blit(game_board, (BORDER_WIDTH, HUD_HEIGHT+BORDER_WIDTH))
 
         pygame.display.update()
         clock.tick(FPS)
